1d26c741c6067ef799667f1012ae08977eb944d5
david.hausmann <davidwilliamhausmann@gmail.com>
2025-11-02 20:53:16 -0300

    change projects and scroll styles

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ document.addEventListener("DOMContentLoaded", () => {
       modalElement.open();
     });
   }
+  document
+    .querySelectorAll('nav a[href^="#"], .header a[href^="#"]')
+    .forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: "smooth",
+          });
+        }
+      });
+    });
 });
